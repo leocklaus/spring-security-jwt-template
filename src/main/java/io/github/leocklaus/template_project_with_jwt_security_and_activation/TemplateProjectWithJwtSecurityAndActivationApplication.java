726fafed1fe864b41c2c6bbf4ab6fd608cd32a0f
leocklaus <leocklaus@gmail.com>
2024-08-01 13:07:41 -0300

    feat: project created

---
+++ src/main/java/io/github/leocklaus/template_project_with_jwt_security_and_activation/TemplateProjectWithJwtSecurityAndActivationApplication.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.template_project_with_jwt_security_and_activation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateProjectWithJwtSecurityAndActivationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateProjectWithJwtSecurityAndActivationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/leocklaus/template_project_with_jwt_security_and_activation/TemplateProjectWithJwtSecurityAndActivationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.template_project_with_jwt_security_and_activation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateProjectWithJwtSecurityAndActivationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
