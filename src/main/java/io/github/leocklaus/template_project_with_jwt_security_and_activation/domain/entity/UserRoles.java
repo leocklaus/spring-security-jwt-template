617466be53e3ef341b991e7ee6f59f186be2c107
leocklaus <leocklaus@gmail.com>
2024-08-02 15:56:03 -0300

    feat: user roles

---
+++ src/main/java/io/github/leocklaus/template_project_with_jwt_security_and_activation/domain/entity/UserRoles.java
@@ -0,0 +1,16 @@
+package io.github.leocklaus.template_project_with_jwt_security_and_activation.domain.entity;
+
+public enum UserRoles {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRoles(String role) {
+        this.role = role;
+    }
+
+    public String getRole(){
+        return role;
+    }
+}
