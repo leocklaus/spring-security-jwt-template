b96d41ec0ee315988a9c2958951f06619530c864
leocklaus <leocklaus@gmail.com>
2024-08-02 15:55:08 -0300

    feat: async enabled

--- src/main/java/io/github/leocklaus/template_project_with_jwt_security_and_activation/TemplateProjectWithJwtSecurityAndActivationApplication.java
+++ src/main/java/io/github/leocklaus/template_project_with_jwt_security_and_activation/TemplateProjectWithJwtSecurityAndActivationApplication.java
@@ -2,8 +2,10 @@ package io.github.leocklaus.template_project_with_jwt_security_and_activation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class TemplateProjectWithJwtSecurityAndActivationApplication {
 
 	public static void main(String[] args) {
